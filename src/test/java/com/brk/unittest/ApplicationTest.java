e8f69d596341a77469e66ed5453bc4414a9ab8af
Rama <brk.bathula@gmail.com>
2020-12-13 18:19:22 -0500

    from eclipse

--- src/test/java/com/brk/unittest/ApplicationTest.java
+++ src/test/java/com/brk/unittest/ApplicationTest.java
@@ -1,8 +1,11 @@
-package com.brk.training;
+package com.brk.unittest;
 
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import com.brk.training.Application;
+
 import static org.junit.Assert.*;
 
 public class ApplicationTest {
