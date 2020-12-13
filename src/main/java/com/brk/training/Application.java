52fcf38a68e99039a5e23c082e9cd242853c3968
Rama <brk.bathula@gmail.com>
2020-12-13 16:55:21 -0500

    Use Maven Dependencies

--- src/main/java/com/brk/training/Application.java
+++ src/main/java/com/brk/training/Application.java
@@ -1,7 +1,17 @@
 package com.brk.training;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Application {
     
+	public int countWords(String words){
+	
+		String[] separateWords = StringUtils.split(words,' ');
+		return (separateWords ==null)? 0 : separateWords.length;
+	
+	}
+	
+	
     public Application() {
         System.out.println ("Inside Application");
     }
@@ -9,6 +19,8 @@ public class Application {
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
+		Application app = new Application();
+		int count = app.countWords("I have four words");
+		System.out.println("Word count :"+ count);
     }
 }
